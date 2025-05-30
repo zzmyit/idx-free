4219e3859441c852f9d050085dda5182440879ef
zzmyit <71055651+zzmyit@users.noreply.github.com>
2025-05-30 16:49:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 npm install uuid
 
 # 使用 Node.js 脚本生成 UUID 并赋值给环境变量
-export UUID=$(node -e "const { v4: uuidv4 } = require('277cc408-95e1-40f2-9caf-caa739702269'); console.log(uuidv4());")
+export UUID="277cc408-95e1-40f2-9caf-caa739702269" # uuid，
 
 # 调试输出：确认 UUID 已生成
 echo "Generated UUID: $UUID"
